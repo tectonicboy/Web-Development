8c077759f5d84a29d0ca5d1a7bffa941298b11fb
tectonicboy <49075948+tectonicboy@users.noreply.github.com>
2021-02-21 00:00:00 +0200

    Update http_server.c

--- BIG_PROJECT/http_server.c
+++ BIG_PROJECT/http_server.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <math.h>
 #include <assert.h>
+#include <unistd.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -10,9 +11,10 @@
 #include <netinet/in.h>
 
 #define MESSAGE_BUFFER_SIZE 1024
+#define TIME_SECOND 1000000
 
-size_t ReadFile(char* filePath, char** buffer) {
-    FILE *fd = fopen(filePath, "r");
+size_t ReadFile(char* filePath, char** buffer, char* mode) {
+    FILE *fd = fopen(filePath, mode);
 
     if (!fd) {
         printf("Could'nt open file\n");
@@ -20,6 +22,7 @@ size_t ReadFile(char* filePath, char** buffer) {
     }
     fseek(fd, 0, SEEK_END);
     size_t numBytes = ftell(fd);
+    printf("Read the file %s, and its size in bytes is: %lu\n", filePath, numBytes);
     fseek(fd, 0, SEEK_SET);
 
     if (numBytes == 0) {
@@ -71,35 +74,39 @@ unsigned Extract_HTTP_Variables(char* client_message, char* serverside_variables
 	int counter = 0;
         unsigned i = 0;
         unsigned offset = 0;
+	int found = 1;
     	while((i < 1024) && !((client_message[i] == 'a') && (client_message[i+1] == 'c') && (client_message[i+2] == 't')
 	 && (client_message[i+3] == 'i') && (client_message[i+4] == 'o') 
 	 && (client_message[i+5] == 'n') && (client_message[i+6] == '=')))
     	{
     		++i;
     	}
-    	printf("Found the position at which server side variables begin: i = %d\n", i);
-    	for(unsigned j = i; client_message[j] != '\0';){
-    		++counter;
-    		if((client_message[j] == '%') && (client_message[j+1] == '4') && (client_message[j+2] == '0')){
-    			serverside_variables[j - i - offset] = hexdec(client_message[j+1], client_message[j+2]);
-    			j += 3;
-			offset += 2;
-    		}
-		if(client_message[j] == '&'){
-			serverside_variables[j - i - offset] = '-';
-			++j;
-		}
-    		else{
-    			serverside_variables[j - i - offset] = client_message[j];
-    			++j;
-    		}
-		if(client_message[j] == '\0'){
-			serverside_variables[j - i - offset] = '\n';
-		}
-		printf("About to check the character: %c, following to write in serverside_variables[%d-%d]\n", client_message[j], j, i);
-		
-    	}
-    	printf("Looped in server side variables string for %d times before encountering null character\n", counter);
+	if(i < 1024){printf("Found the position at which server side variables begin: i = %d\n", i);}
+    	else {printf("No server side variables in this http request.\n"); found = 0;}
+	if(found){
+	    	for(unsigned j = i; client_message[j] != '\0';){
+	    		++counter;
+	    		if((client_message[j] == '%') && (client_message[j+1] == '4') && (client_message[j+2] == '0')){
+	    			serverside_variables[j - i - offset] = hexdec(client_message[j+1], client_message[j+2]);
+	    			j += 3;
+				offset += 2;
+	    		}
+			if(client_message[j] == '&'){
+				serverside_variables[j - i - offset] = '-';
+				++j;
+			}
+	    		else{
+	    			serverside_variables[j - i - offset] = client_message[j];
+	    			++j;
+	    		}
+			if(client_message[j] == '\0'){
+				serverside_variables[j - i - offset] = '\n';
+			}
+			printf("About to check the character: %c, following to write in serverside_variables[%d-%d]\n", client_message[j], j, i);
+			
+	    	}
+	}
+    	if(counter > 0){printf("Looped in server side variables string for %d times before encountering null character\n", counter);}
 	return i;
 }
 
@@ -157,9 +164,20 @@ void SQL_Command_Constructor(char* vars, FILE* cmd_output){
 
 int main(){
     int server_socket, client_socket, clientLen, read_size, port = 80;
+    char* r_mode = "r"; char* rb_mode = "rb";
+
+    char* http_header_bgimg = "HTTP/1.1 200 OK\n\rContent-Type: image/jpg\n\rContent-Length: 31623\r\n\n";
+    char* bgimg_buf = calloc(1, 31623);
+    char* bgimg_name = "pic5.jpg";
+    size_t bgimg_siz = ReadFile(bgimg_name, &bgimg_buf, rb_mode);
+    char* bgimg_response_buf = calloc(1, (strlen(http_header_bgimg) + bgimg_siz));  
+    strcpy(bgimg_response_buf, http_header_bgimg);
+    memcpy((bgimg_response_buf + strlen(http_header_bgimg)), bgimg_buf, bgimg_siz);
+
     char* http_header = "HTTP/1.1 200 OK\r\n\n";
     char* response_data;
-    if((posix_memalign((void*)&response_data, 64, 65536)) != 0){
+
+    if(posix_memalign((void*)&response_data, 64, 65536) != 0){
 	printf("Unable to allocate 64-byte aligned memory for html buffer.\n");
     }
     char* client_message;
@@ -168,19 +186,18 @@ int main(){
     }	
 
     struct sockaddr_in server_address, client_address;
-    char* fname = "index.php";
-
-    size_t f_siz = ReadFile(fname, &response_data);
+    char* fname = "index.html";
+    size_t f_siz = ReadFile(fname, &response_data, r_mode);
 
     printf("We have read file contents: %s\n", response_data);
-	
+
+    //Combine the header and the actual body by concatenating into one string.
     char* HTTP_response;
     if((posix_memalign((void*)&HTTP_response, 64, (strlen(http_header) + strlen(response_data) + 1))) != 0){
 	printf("Unable to allocate 64-byte aligned memory for HTTP response buffer.\n");
     }
     strcpy(HTTP_response, http_header);
     strcat(HTTP_response, response_data);
-	
     //create a socket
 
     //same params as TCP one.
@@ -201,29 +218,70 @@ int main(){
     if((posix_memalign((void*)&serverside_variables, 64, MESSAGE_BUFFER_SIZE) ) != 0){
 	printf("Unable to allocate 64-byte aligned memory for serverside variables buffer.\n");
     }
-    //We wrap in an infinite while loop so the server can continue serving and responding to requests forever.
     unsigned servervars_start_i;
+    char* requested_fname;
+    if(posix_memalign((void*)&requested_fname, 64, 64) != 0){
+	printf("Unable to allocate 64-byte aligned memory for http requested file name buffer.\n");
+    }
+    int sent;
+    u_int32_t imageResponseLen;
+    size_t k;
+    //We wrap in an infinite while loop so the server can continue serving and responding to requests forever.
     while(1){
-    	printf("Listening for HTTP requests on port %d...\n", port);
+	sent = 0;
+    	printf("\n\nListening for HTTP requests on port %d...\n", port);
     	clientLen = sizeof(struct sockaddr_in);
         client_socket = accept(server_socket, (struct sockaddr*) &client_address, (socklen_t*) &clientLen);
-	//We are zeroing them with memset() because these will change with every different HTTP client request!!
 	memset(client_message, 0x0, MESSAGE_BUFFER_SIZE);
     	memset(serverside_variables, 0x0, MESSAGE_BUFFER_SIZE);
+	memset(requested_fname, 0x0, 64);
     	if(recv(client_socket, client_message, 1024, 0) < 0){
     		printf("Something went wrong while receiving client's HTTP request.\n");
     		break;
     	}
-    	printf("Client reply: %s\n", client_message);
-    	sql_command_file = fopen("SERVER_SQL_COMMANDS.txt", "a");
-	if(!(sql_command_file)){printf("Unable to open SQL command file.\n");}
+    	printf("\n****** Received an HTTP request ****** \n%s\n", client_message);
+        k = 5;
+	while(!(client_message[k+1] == 'H' && client_message[k+2] == 'T' 
+		&& client_message[k+3] == 'T' && client_message[k+4] == 'P'))
+	{
+		requested_fname[k-5] = client_message[k];
+		++k;
+		if(k > 64){break;}
+	}
+	if(strlen(requested_fname)){
+		printf("The client requested a file called: %s\n", requested_fname);
+		if(!strcmp(bgimg_name, requested_fname)){
+			imageResponseLen = (strlen(http_header_bgimg) + bgimg_siz);
+			if(send(client_socket, bgimg_response_buf, imageResponseLen, 0) < 0){
+				printf("bgimg sending failed.\n");
+			}
+			else{sent = 1;}
+		}
+	}
+	printf("After dealin with requested file name, sent = %d\n", sent);
+
 	servervars_start_i = Extract_HTTP_Variables(client_message, serverside_variables);
-	printf("After extracting HTTP variables, the string looks like this: %s\n", serverside_variables);
-	if(servervars_start_i < MESSAGE_BUFFER_SIZE){SQL_Command_Constructor(serverside_variables, sql_command_file);}
-    	fclose(sql_command_file);
-        if(send(client_socket, HTTP_response, strlen(HTTP_response), 0) < 0){
-    		printf("Send failed.\n");
-    	}
+	if(strlen(serverside_variables)){
+		printf("Extracted HTTP variables string: %s\n", serverside_variables);
+	}
+	if(servervars_start_i < MESSAGE_BUFFER_SIZE){
+		l2:
+	    	sql_command_file = fopen("SERVER_SQL_COMMANDS.txt", "a");
+		if(!(sql_command_file)){
+			printf("Unable to open SQL command file for appending. Retrying...\n");
+			usleep(0.2 * TIME_SECOND);
+			goto l2;
+		}
+		SQL_Command_Constructor(serverside_variables, sql_command_file);
+		fclose(sql_command_file);	
+	}
+        if(!sent){
+		printf("About to send webpage code.\n");
+		if(send(client_socket, HTTP_response, strlen(HTTP_response), 0) < 0){
+	    		printf("html/css/js code sending failed.\n");
+	    	}
+	}
+
         close(client_socket);
     }
     printf("About to return 0 and exit.\n");
